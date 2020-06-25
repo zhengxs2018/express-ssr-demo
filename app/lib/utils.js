60b6617a899bcc59efc80c7244ae5fa71c55f9d1
zhengxs <zhengxs2018@gmail.com>
2020-06-25 20:11:46 +0800

    添加密码 hash 功能

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -2,6 +2,32 @@
 
 const { existsSync, statSync } = require('fs')
 
+const config = require('config')
+
+const { hash, compare } = require('bcryptjs')
+
+/**
+ * This function is responsible of hashing the password test.
+ *
+ * @param {String} text the text to hash
+ * @returns {String} the hashed string
+ */
+function hashPassword (text) {
+  return hash(text, config.get('PASSWORD_HASH_SALT_LENGTH'))
+}
+
+/**
+ * Validate that the hash is actually the hashed value of plain text
+ *
+ * @param {String} password   the password to validate
+ * @param {String} hash   the hash to validate
+ * @returns {Boolean} whether the password hash is valid
+ */
+async function validatePasswordHash (password, hash) {
+  return compare(password, hash)
+}
+
+
 function isString(value) {
   return typeof value === 'string'
 }
@@ -25,6 +51,9 @@ function isNpmPackage(value) {
 }
 
 module.exports = {
+  hashPassword,
+  validatePasswordHash,
+
   isString,
   isFile,
   isNodeModule,
