a8c57849857f0dc5b6a9be30e5c73f8a65576f76
zhengxs <zhengxs2018@gmail.com>
2020-06-26 13:42:42 +0800

    添加 Appointment 数据模型

--- scripts/clear-db.js
+++ scripts/clear-db.js
@@ -8,10 +8,12 @@ const connectDB = require('../database/connect')
 logger.info('Clear database tables.')
 
 const clearDB = async () => {
+  const Appointment = require('../app/models/Appointment')
   const User = require('../app/models/User')
   const VerificationCode = require('../app/models/VerificationCode')
 
   // clear database tables
+  await Appointment.deleteMany({})
   await User.deleteMany({})
   await VerificationCode.deleteMany({})
 }
