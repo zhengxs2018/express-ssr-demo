bab3e83babb172815a8ab198cc40f68492fe0d40
zhengxs <zhengxs2018@gmail.com>
2020-06-26 17:21:58 +0800

    添加数据模型

---
+++ app/models/Schedule.js
@@ -0,0 +1,24 @@
+const config = require('config')
+const { Schema } = require('mongoose')
+
+const { fieldEncryption } = require('mongoose-field-encryption')
+
+const defineModel = require('../../database/model')
+
+/**
+ * the user Schedule
+ * this used to record
+ */
+const schema = new Schema({
+  userId: String,
+  key: String,
+  slots: [{ start: String, end: String }]
+})
+
+schema.plugin(fieldEncryption, {
+  fields: Object.keys(schema.obj),
+  secret: config.get('ENCRYPTION_SECRET_KEY'),
+  saltGenerator: secret => secret.slice(0, 16),
+})
+
+module.exports = defineModel('Schedule', schema)
